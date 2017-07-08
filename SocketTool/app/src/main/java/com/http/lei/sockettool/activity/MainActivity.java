53417351bfdd10536f8211448cb095286e7f2eec
jialeilei <public_lei@126.com>
2017-07-08 14:52:45 +0800

    更新

--- SocketTool/app/src/main/java/com/http/lei/sockettool/activity/MainActivity.java
+++ SocketTool/app/src/main/java/com/http/lei/sockettool/activity/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
-        
+
         btnTcp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
